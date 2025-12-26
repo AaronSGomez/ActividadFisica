872038d55fe7a21b269f3061f6c24429dc8a9a68
Aarón Gómez <nomeacuerdobien@gmail.com>
2025-12-26 21:02:22 +0100

    ya funciona la primera parte

--- app/src/main/java/com/example/actividadfisica/MainActivity.kt
+++ app/src/main/java/com/example/actividadfisica/MainActivity.kt
@@ -1,26 +1,18 @@
 package com.example.actividadfisica
 
 import android.os.Bundle
-import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
-import android.widget.ImageView
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.actividadfisica.model.Registro  //importar clase Registro
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.ui.Modifier
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.actividadfisica.ui.theme.ActividadFisicaTheme
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.Date
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,8 +25,17 @@ class MainActivity : ComponentActivity() {
         val tvTitulo = findViewById<TextView>(R.id.tvTitulo)
         val etActivity = findViewById<EditText>(R.id.etActivity)
         val etDuration = findViewById<EditText>(R.id.etDuration)
-        val etType = findViewById<EditText>(R.id.etType)
+        val spType = findViewById<Spinner>(R.id.spType)
         val btnSave = findViewById<Button>(R.id.btnSave)
+
+        val opcionesDeporte = listOf("Cardio", "Fuerza / Tonificación", "HIIT / Alta Intensidad","Natación",
+            "Aerobicos / Spinning",
+            "Movilidad", "Yoga / Pilates", "Artes Marciales","Outdoor",
+            "Otros")
+        val adapterSpinner =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, opcionesDeporte)
+        adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spType.adapter = adapterSpinner
 
         val rv = findViewById<RecyclerView>(R.id.rvActivities)
 
@@ -49,7 +50,7 @@ class MainActivity : ComponentActivity() {
         btnSave.setOnClickListener {
             val activity = etActivity.text.toString().trim()
             val duration = etDuration.text.toString().trim().toIntOrNull()
-            val type = etType.text.toString().trim()
+            val type = spType.selectedItem.toString()
 
             val timeNow = LocalDateTime.now()   // cogemos la fecha y hora del sistema
             val format = DateTimeFormatter.ofPattern("dd/MM HH:mm")   // formateamos
@@ -66,7 +67,7 @@ class MainActivity : ComponentActivity() {
 
             etActivity.text.clear()
             etDuration.text.clear()
-            etType.text.clear()
+            spType.setSelection(0)
 
         }
 
